4c19fb14b2a6438ca173af3c60e248b63b56705f
Ryanmh1024 <196107143+Ryanmh1024@users.noreply.github.com>
2026-01-15 20:14:48 +0000

    updated instructions

--- howtonewproject.py
+++ howtonewproject.py
@@ -3,5 +3,5 @@
 # Step 2: Make a codespace and open it in VS Code
 # Step 3: Create a virtual environment
 # Step 4: Create a requirements.txt file for all packages
-# Step 4: Create files in your new working environment
-# Step 5: Finish creating what project you want and then commit files to the github repo
+# Step 5: Create files in your new working environment
+# Step 6: Finish creating what project you want and then commit files to the github repo
